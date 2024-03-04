e6268592625befbdc72323aa450c76d56b55231e
Be <be@c24>
2024-03-04 01:55:08 +0100

    blacklists_updater.sh

--- blacklists_updater.sh
+++ blacklists_updater.sh
@@ -10,7 +10,7 @@ grep -iE "uvd|fgup|grchc|roskomnad" auto/all-ru-asn.txt | awk '{ print "# AS-Nam
 ./network_list_from_as.py ${auto_black_ass} > ${outfile_w_comments}
 ./network_list_from_netname.py lists/ru-gov-netnames.txt >> ${outfile_w_comments}
 grep -iE "uvd|fgup|grchc|roskomnad" auto/all-ru-ipv4.txt  | awk '{ print "# AS-Name: " $0 "\n" $1}' >> ${outfile_w_comments}
-grep -iE "uvd|fgup|grchc|roskomnad" auto/ripe-ru-ipv4.txt | awk '{ print "# AS-Name: " $0 "\n" $1}' >> ${outfile_w_comments}
+grep -iE "uvd|fgup|grchc|roskomnad|federalnaya sluzhba|ufsb" auto/ripe-ru-ipv4.txt | awk '{ print "# AS-Name: " $0 "\n" $1}' >> ${outfile_w_comments}
 
 # Remove comments by sed to avoid flooding WHOIS servers
 grep -v "#" ${outfile_w_comments} | sort | uniq > ${outfile_wo_comments}
