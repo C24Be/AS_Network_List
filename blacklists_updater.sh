6fc02bea2dcc86ed9bdc63939ef9a91964cb9238
C24Be <c24@be>
2024-04-01 12:02:00 +0200

    update blacklists_updater.sh regexps

--- blacklists_updater.sh
+++ blacklists_updater.sh
@@ -8,10 +8,11 @@ auto_black_ass="auto/black_ass.txt"
 black_names="uvd|umvd|fgup|grchc|roskomnad|federalnaya sluzhba|ufsb|zonatelecom"
 white_names="ruvds"
 
-grep -iE "${black_names}" auto/all-ru-asn.txt | grep -viE "${white_names}" | awk '{ print "# AS-Name: " $0 "\n" $1}' > ${auto_black_ass}
+grep -iE "${black_names}" auto/all-ru-asn.txt   | grep -viE "${white_names}" | awk '{ print "# AS-Name: " $0 "\n" $1}' > ${auto_black_ass}
 ./network_list_from_as.py ${auto_black_ass} > ${outfile_w_comments}
 ./network_list_from_netname.py lists/ru-gov-netnames.txt >> ${outfile_w_comments}
-grep -iE "${black_names}" auto/*-ru-ipv4.txt  | grep -viE "${white_names}" | awk '{ print "# NET-Name: " $0 "\n" $1}' >> ${outfile_w_comments}
+grep -iE "${black_names}" auto/all-ru-ipv4.txt  | grep -viE "${white_names}" | awk '{ print "# NET-Name: " $0 "\n" $1}' >> ${outfile_w_comments}
+grep -iE "${black_names}" auto/ripe-ru-ipv4.txt | grep -viE "${white_names}" | awk '{ print "# NET-Name: " $0 "\n" $1}' >> ${outfile_w_comments}
 
 # Remove comments by sed to avoid flooding WHOIS servers
 grep -v "#" ${outfile_w_comments} | sort | uniq > ${outfile_wo_comments}
